090a338d312465c12ef398bdfe86ef2c688813ca
axel-iradukunda <axel.iradukunda@cgi.com>
2025-01-07 09:18:52 +0100

    Add constants in test cases to improve readability

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,10 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private static final String DEXTERITY_VEST = "+5 Dexterity Vest";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+
     // Test the behavior of a regular item
     @Test
     void testRegularItem() {
-        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
+        Item[] items = new Item[] { new Item(DEXTERITY_VEST, 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -19,7 +25,7 @@ class GildedRoseTest {
     // Test the behavior of "Aged Brie" which increases in quality as it ages
     @Test
     void testAgedBrie() {
-        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        Item[] items = new Item[] { new Item(AGED_BRIE, 2, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(1, app.items[0].sellIn);
@@ -29,7 +35,7 @@ class GildedRoseTest {
     // Test the behavior of "Sulfuras" which never decreases in quality or needs to be sold
     @Test
     void testSulfuras() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        Item[] items = new Item[] { new Item(SULFURAS, 0, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(0, app.items[0].sellIn);
@@ -39,7 +45,7 @@ class GildedRoseTest {
     // Test the behavior of "Backstage passes" which increase in quality as the concert date approaches
     @Test
     void testBackstagePasses() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, 15, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(14, app.items[0].sellIn);
@@ -49,7 +55,7 @@ class GildedRoseTest {
     // Test the behavior of "Backstage passes" when there are 10 days or less until the concert
     @Test
     void testBackstagePassesWith10DaysOrLess() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -59,7 +65,7 @@ class GildedRoseTest {
     // Test the behavior of "Backstage passes" when there are 5 days or less until the concert
     @Test
     void testBackstagePassesWith5DaysOrLess() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, 5, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(4, app.items[0].sellIn);
@@ -69,7 +75,7 @@ class GildedRoseTest {
     // Test the behavior of "Backstage passes" after the concert date has passed
     @Test
     void testBackstagePassesAfterConcert() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        Item[] items = new Item[] { new Item(BACKSTAGE_PASSES, 0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
@@ -79,7 +85,7 @@ class GildedRoseTest {
     // Test the behavior of "Conjured" items which degrade in quality twice as fast as normal items
     @Test
     void testConjuredItem() {
-        Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        Item[] items = new Item[] { new Item(CONJURED_MANA_CAKE, 3, 6) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(2, app.items[0].sellIn);
@@ -89,7 +95,7 @@ class GildedRoseTest {
     // Test the behavior of "Conjured" items after the sell-by date has passed
     @Test
     void testConjuredItemAfterSellIn() {
-        Item[] items = new Item[] { new Item("Conjured Mana Cake", 0, 6) };
+        Item[] items = new Item[] { new Item(CONJURED_MANA_CAKE, 0, 6) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
